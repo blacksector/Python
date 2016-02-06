54c990cd5d01cbbf1c840224470fef826f0bcedb
pythogen <pythogen@wayne.cool>
2016-02-06 14:04:53 -0500

    Reference Sample

---
+++ Reference/Custom_Dictionary_Cycle.py
@@ -0,0 +1,61 @@
+# Pythogen
+
+# Reference: Three passwords per username Cycle / Switch to next three passwords in the list after full username Cycle
+
+# Three passwords per username Cycle
+# Specific pattern for username specific rate limits
+# Enable greater time duration between specific usernames
+# while still cycling all passwords.
+# Three password attempts at a time per username,
+# Any size password list and every user:password combo will 
+# be attempted.
+
+# Example [ Pattern illustrated ]:
+# user1:pass1
+# user1:pass2
+# user1:pass3
+# user2:pass1
+# user2:pass2
+# user2:pass3
+# user3:pass1, etc...
+# ...
+# user1:pass4
+# user1:pass5
+# user1:pass6
+# user2:pass4
+# user2:pass5, etc....
+
+
+import time
+
+# Variables for indice control
+a = 0
+b = 3
+
+def Cycle():
+	# Keep slice values accessable
+	global a
+	global b
+	# Represents user values
+	user = ['Mike','shawn','Ted']
+	# Represents password values
+	pw = ['a','aa','aaa','b', 'bb', 'bbb', 'c', 'cc', 'ccc']
+
+	# User cycle
+	for u in user:
+		# Password cycle with pw slice
+		# Slice for specific pw range per user
+		for p in pw[a:b]:
+			# Print process
+			print '%s:%s' % (u,p)
+			time.sleep(1)
+
+	# Slice variables increment
+	a = a + 3
+	b = b + 3
+	# Recursive
+	Cycle()
+
+Cycle()
+# Press enter to exit
+x = raw_input("")
