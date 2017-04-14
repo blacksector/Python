bf4f4ba4e0ce5347a55d94eecbd7ee2bc9563f9f
Omar Quazi <omarquaz@gmail.com>
2017-04-14 18:16:13 -0400

    Update USEcrypt.py
    Rewrote encrypt and decrypt functions to make it simpler. Added extra command to exit.
    
    Caesar cipher simply adds 1 place value to the letter in this program, so convert character into ASCII number value, add 1 and convert the changed number back into a character.

--- Projects/USEcrypt/USEcrypt.py
+++ Projects/USEcrypt/USEcrypt.py
@@ -10,188 +10,67 @@ def addToClipBoard(text):
     os.system(command)
 
 def decr():
-	# Declare input string
-	x = raw_input("\nEnter Message to Decrypt: ")
+    # Declare input string
+    x = raw_input("\nEnter Message to Decrypt: ").lower()
 
-	# Array declared
-	ar = []
+    # Final string
+    decrypted = ""
 
-	# For every Character in input string
-	for c in x:
-		n = ord(c)
-		# if 'a' contained in input string then..
-		if c == "a":
-			# then append to array
-			ar.append("z")
-		elif c == "b":
-			ar.append('a')
-		elif c == "c":
-			ar.append("b")
-		elif c == "d":
-			ar.append("c")
-		elif c == "e":
-			ar.append("d")
-		elif c == "f":
-			ar.append("e")
-		elif c == "g":
-			ar.append("f")
-		elif c == "h":
-			ar.append("g")
-		elif c == "i":
-			ar.append("h")
-		elif c == "j":
-			ar.append("i")
-		elif c == "k":
-			ar.append("j")
-		elif c == "l":
-			ar.append("k")
-		elif c == "m":
-			ar.append("l")
-		elif c == "n":
-			ar.append("m")
-		elif c == "o":
-			ar.append("n")
-		elif c == "p":
-			ar.append("o")
-		elif c == "q":
-			ar.append("p")
-		elif c == "r":
-			ar.append("q")
-		elif c == "s":
-			ar.append("r")
-		elif c == "t":
-			ar.append("s")
-		elif c == "u":
-			ar.append("t")
-		elif c == "v":
-			ar.append("u")
-		elif c == "w":
-			ar.append("v")
-		elif c == "x":
-			ar.append("w")
-		elif c == "y":
-			ar.append("x")
-		elif c == "z":
-			ar.append("y")
-		elif c == "0":
-			ar.append(" ")
-		elif c == "1":
-			ar.append(",")
-		elif c == "2":
-			ar.append(".")
-		elif c == "3":
-			ar.append("'")
-		elif c == "4":
-			ar.append("?")
-		elif c == "5":
-			ar.append("!")
+    # For every Character in input string
+    for c in x:
+        # Uses Caesar cipher of -1 to every letter
+        val = ord(c)-1
 
+        # ASCII values have a limit of 255,
+        # so we will simply wrap the characters around
+        if val < 0:
+            decrypted += chr(255-val)
+        else:
+            decrypted += chr(val)
 
-	# Display all appended in array
-	decrypted = ''.join(ar)
-
-	# Display encrypted Text
-	print "\nYour decrypted text: %s" % (decrypted)
+    # Display encrypted Text
+    print "\nYour decrypted text: %s" % (decrypted)
 
 
 
 def encr():
-	# Declare input string
-	x = raw_input("\nEnter Message to Encrypt: ")
+    # Declare input string
+    x = raw_input("\nEnter Message to Encrypt: ").lower()
 
-	# Array declared
-	ar = []
+    # Final string
+    encrypted = ""
 
-	# For every Character in input string
-	for c in x:
-		n = ord(c)
-		# if 'a' contained in input string then..
-		if c == "a" or c == "A":
-			# then append to array
-			ar.append('b')
-		elif c == "b" or c == "B":
-			ar.append("c")
-		elif c == "c" or c == "C":
-			ar.append("d")
-		elif c == "d" or c == "D":
-			ar.append("e")
-		elif c == "e" or c == "E":
-			ar.append("f")
-		elif c == "f" or c == "F":
-			ar.append("g")
-		elif c == "g" or c == "G":
-			ar.append("h")
-		elif c == "h" or c == "H":
-			ar.append("i")
-		elif c == "i" or c == "I":
-			ar.append("j")
-		elif c == "j" or c == "J":
-			ar.append("k")
-		elif c == "k" or c == "K":
-			ar.append("l")
-		elif c == "l" or c == "L":
-			ar.append("m")
-		elif c == "m" or c == "M":
-			ar.append("n")
-		elif c == "n" or c == "N":
-			ar.append("o")
-		elif c == "o" or c == "O":
-			ar.append("p")
-		elif c == "p" or c == "P":
-			ar.append("q")
-		elif c == "q" or c == "Q":
-			ar.append("r")
-		elif c == "r" or c == "R":
-			ar.append("s")
-		elif c == "s" or c == "S":
-			ar.append("t")
-		elif c == "t" or c == "T":
-			ar.append("u")
-		elif c == "u" or c == "U":
-			ar.append("v")
-		elif c == "v" or c == "V":
-			ar.append("w")
-		elif c == "w" or c == "W":
-			ar.append("x")
-		elif c == "x" or c == "X":
-			ar.append("y")
-		elif c == "y" or c == "Y":
-			ar.append("z")
-		elif c == "z" or c == "Z":
-			ar.append("a")
-		elif c == " ":
-			ar.append("0")
-		elif c == ",":
-			ar.append("1")
-		elif c == ".":
-			ar.append("2")
-		elif c == "'":
-			ar.append("3")
-		elif c == "?":
-			ar.append("4")
-		elif c == "!":
-			ar.append("5")
+    # For every Character in input string
+    for c in x:
+        # Uses Caesar cipher of +1 to every letter
+        val = ord(c)+1
 
-	# Display all appended in array
-	encrypted = ''.join(ar)
+        # ASCII values have a limit of 255,
+        # so we will simply wrap the characters around
+        if val > 255:
+            encrypted += chr(val-255)
+        else:
+            encrypted += chr(val)
 
-	# Display encrypted Text
-	print "\nYour encrypted text: %s" % (encrypted)
+    # Display encrypted Text
+    print "\nYour encrypted text: %s" % (encrypted)
 
-	print "\nEncrypted text copied to clipboard.\n"
-	# Copy text to clipboard
-	addToClipBoard(encrypted)
+    print "\nEncrypted text copied to clipboard.\n"
+    # Copy text to clipboard
+    addToClipBoard(encrypted)
 
 
 while(True):
-	
-	cmd = raw_input("Enter command (? for help): ")
+    
+    cmd = raw_input("Enter command (? for help): ").lower()
 
-	if cmd == "encrypt":
-		encr()
-	elif cmd == "decrypt":
-		decr()
-	elif cmd == "?" or cmd == "help":
-		print "\n Commands:\n\n encrypt (input for encryption)\n decrypt (input for decryption\n"
-	else:	
-		print "\n [!] Invalid command. Try again. \n"
+    if cmd == "encrypt":
+        encr()
+    elif cmd == "decrypt":
+        decr()
+    elif cmd == "exit":
+        exit()
+    elif cmd == "?" or cmd == "help":
+        print "\n Commands:\n\n encrypt\t(input for encryption)\n decrypt\t(input for decryption)\n exit\t\t(input to exit program)\n"
+    else:   
+        print "\n [!] Invalid command. Try again. \n"
